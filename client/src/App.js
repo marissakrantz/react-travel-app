d7180c3a7b2bf92d6c373618f245dce552946c8e
Marissa Tobin <marissatobin@192-168-1-102.tpgi.com.au>
2022-10-24 01:00:34 +0800

    new app - change react versions back to newest

---
+++ client/src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Header from '../src/components/Header';
+import Footer from '../src/components/Footer';
+import Home from '../src/pages/Home';
+import Login from '../src/pages/Login';
+import Signup from '../src/pages/SignUp'
+import SingleTrip from '../src/pages/SingleTrip'
+
+import { ChakraProvider } from "@chakra-ui/react"
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+        <ChakraProvider>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/trip/:tripId" element={<SingleTrip />} />
+          </Routes>
+          <Footer />
+        </ChakraProvider>
+        </BrowserRouter>
+      </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,76 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+  query user($userID: ID!) {
+    user(userID: $userID) {
+      username
+      email
+      trips {
+        _id
+        name
+        completed
+      }
+    }
+  }
+`;
+
+export const QUERY_TRIPS = gql`
+query getTrips {
+    trips {
+        _id
+        name
+        completed
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      trips {
+        _id
+        name
+        completed
+      }
+    }
+  }
+`;
+
+export const QUERY_ITINERARY = gql`
+query getItinerary {
+    itinerary {
+        _id
+        name
+        completed
+        startDate
+        endDate
+        details
+    }
+    trips {
+      _id
+      name
+      completed
+    }
+  }
+`;
+
+export const QUERY_SINGLE_TRIP = gql`
+  query getSingleTrip($tripsId: ID!) {
+    trip(tripsId: $tripsId) {
+      _id
+      name
+      completed
+      itinerary {
+        _id
+        name
+        completed
+        startDate
+        endDate
+        details
+      }
+    }
+  }
+`
