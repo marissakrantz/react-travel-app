4ee7ef946ae2af56b8193c1671b7ed4a8bd7c5c7
Marissa Tobin <marissatobin@192-168-1-102.tpgi.com.au>
2022-10-24 10:01:22 +0800

    connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/TravelDB',
+  process.env.MONGODB_URI || 'mongodb://localhost:3000/TravelDB',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
