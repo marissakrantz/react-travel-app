83148a3b0945a59b80f89fdcfbec49bd39fd9542
Marissa Tobin <marissatobin@192-168-1-102.tpgi.com.au>
2022-10-24 15:55:27 +0800

    add proxy to fix db connection

--- server/server.js
+++ server/server.js
@@ -7,8 +7,9 @@ const { typeDefs, resolvers } = require('./schemas');
 
 const db = require('./config/connection');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
